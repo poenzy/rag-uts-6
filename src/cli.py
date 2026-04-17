75488ee7587a0de84cf5b9e059d39f0d255f8538
OHGMA <gantenghnf@gmail.com>
2026-04-17 11:13:57 +0700

    update requirement

--- src/cli.py
+++ src/cli.py
@@ -4,7 +4,7 @@ from query import get_answer
 def main():
     # Tampilan Header Terminal
     print("="*60)
-    print("🌱CULTIVAGUIDE AI")
+    print("🌱 CULTIVAGUIDE AI")
     print("="*60)
 
     # Looping interaksi tanya jawab
@@ -14,14 +14,14 @@ def main():
         
         # Cek jika user ingin keluar dari program
         if user_input.lower() in ['keluar', 'exit', 'quit', 'q']:
-            print("\n🌱Terima kasih telah menggunakan CultivaGuide. Selamat bertani! \n")
+            print("\n🌱 Terima kasih telah menggunakan CultivaGuide. Selamat bertani! \n")
             break
             
         # Lewati jika input kosong
         if not user_input.strip():
             continue
 
-        print("\n🌱Bentar lagi cari referensi di database dan merumuskan jawaban...")
+        print("\n🌱 Bentar lagi cari referensi di database dan merumuskan jawaban...")
         
         # 2. Panggil fungsi dari query.py
         answer, sources = get_answer(user_input)
